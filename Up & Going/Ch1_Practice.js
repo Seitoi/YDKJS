77f943f858453357ef6cdbd1082d5b8448b3b9d7
Selto <chewedgum12@gmail.com>
2017-05-05 22:50:52 -0400

    Made changes so it can run in atom

--- Up & Going/Ch1_Practice.js
+++ Up & Going/Ch1_Practice.js
@@ -1,12 +1,12 @@
-'.source.js';
+
 
 const TAX_RATE = 0.08
 const PHONE_PRICE = 99.99;
 const ACCESSORY = 9.99;
 const MAX_SPENDING = 150;
-var bank_money
+var bank_money = 150
 
-bank_money = prompt("What is your total worth?");
+//bank_money = prompt("What is your total worth?");
 
 var amount = 0
 
@@ -29,8 +29,8 @@ while (amount < bank_money){
 
 amount = amount + totalTax(amount);		//taxes
 
-console.log( "Your purchase: " + formatAmounmt(amount));
+console.log( "Your purchase: " + formatAmount(amount));
 
 if (amount > bank_money){
-	alert("Sorry, you're broke son")
+	console.log("Sorry, you're broke son")		//alert
 }
