ea14346f394ba92b4067b19bf064789633eb9f0a
Selto <chewedgum12@gmail.com>
2017-05-06 03:06:11 -0400

    30 minute study

---
+++ Up & Going/Chapter 2.js
@@ -0,0 +1,22 @@
+/* The return value of operatior is always
+    one of seven string values  */
+var a;
+typeof a;
+
+a = "hello world";
+typeof a;
+
+a = 42;
+typeof a;
+
+a = true;
+typeof a;
+
+a = null;
+typeof a;
+
+a = undefined;
+typeof a;
+
+a = {b: "c"};
+typeof a;
