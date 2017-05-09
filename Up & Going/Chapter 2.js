213a9c7ba681e17cd180973045aedeeb40d67918
Selto <chewedgum12@gmail.com>
2017-05-09 03:18:12 -0400

    Chapter 2 finished, 35 minute study session

--- Up & Going/Chapter 2.js
+++ Up & Going/Chapter 2.js
@@ -102,3 +102,207 @@ An indentifiers must start with a - z, A - Z, $, or _ then can contain numerals 
 
 However, there are reserved words that can't be used as indentifiers:
 for in  if (functions)  null  true  false */
+
+/* Hoisting, when a variable declaration is taken to the entire scope */
+var a = 2;
+foo();
+
+function foo(){
+  a = 3
+  console.log (a);    //3
+  var a;            //declaration is hoisted to the top of function foo()
+}
+console.log (a); //2
+
+//DO NOT RELY ON HOISTING, much more common practice to
+// use hoisted function declaration (like foo())
+
+/* Nested Scopes - variable is available anywhere in the scope, as well as lower/inner levels */
+function foo() {
+        var a = 1;
+        function bar(){
+          var b = 2;
+          function baz (){
+            var c = 3;
+            console.log (a, b, c);  //1 2 3
+          }
+          baz();
+          console.log (a, b);   //1 2
+        }
+        bar()
+        console.log (a); // 1
+}
+foo();
+
+function foo(){
+        var a = 1;
+        if (a >= 1){
+          //Es6 "lets" you declare variables to belong to certain blocks
+            let b = 2;      //variable "b" will only belong to the if statement, not the whole function foo()
+            while (b < 5) {
+                let c = b *2;
+                b++;
+                console.log (a + c);
+            }
+        }
+}
+foo();  // 5 7 9
+
+/*Condtitionals - if/else vs Switch */
+if (a == 2) {
+  //do something
+}
+else if (a == 10) {
+  //do something
+}
+else if (a == 42) {
+  //do yet another thing
+} else {
+    //fallback to here
+}
+
+switch (a){
+      case 2:
+              //do something
+              break;        // <-- break is important if you want only the statement in one case to run. Otherwise it will run regardless if it matches. (sometimes useful)
+      case 10:
+              //do something else
+              break;
+      case 42:
+              //do yet another thing
+              break;
+      default:
+              //fallack to here
+}
+
+//ternary operator
+var a = 42;
+var b = (a > 41) ? "hello" : "world";   /*  <-- Similar to:     if (a > 41) {
+  if true, first clause "hello" runs                              b = "hello";
+                                                                }
+                                                                else {
+                                                                  b = "world";
+                                                                }
+                                                                                          */
+
+/* Strict Mode */
+//Es5 added strict mode, tightens rules and makes code more optimazable by the engine
+//strict mode follows the nested rules
+function foo(){
+          "use strict";   //Enables strict mode
+          function bar(){
+            //this code is strict mode
+          }
+}
+//this code will not be in strict mode
+//strict mode disallows the implicit auto-global variable declaration (A good thing!)
+//declaring a variable/function is not the same as executing it!
+
+/* Closures */ //<- Very important!
+function makeAdder( x ){
+        //parameter 'x' is an inner variable
+        //inner function 'add()' uses 'x', so
+        //it has a "closure" over it
+        function add( y ) {
+              return y + x;
+        };
+        return add;
+}
+var plusOne = makeAdder ( 1 );      //<-- this function is a reference function
+var plusTen = makeAdder ( 10 );     // it referes back to the inner add() function that remembers x as 1 (plusOne) or 10 (plusTen)
+
+plusOne( 3 );   //4 <-- 1 + 3         When we call pluOne(3) it adds 3 (its inner y) to the 1 (remembered by x) and we get 4
+plusOne( 41 );  //42 <-- 41 + 1
+plusTen ( 13 ); //23 <-- 13 + 10
+
+/* Modules - code that is hidden from the outside but has public API that is accessable from the outside */
+function User(){                                    //<-- outer scope
+      var username, password;
+      function doLogin( user, pw ){           //<-- inner details that cannot be accessed
+          username = user;                  //<-- has closure from doLogin, meaning it will retain its access to them even after function finishes running
+          password = pw;
+          //do the rest of the log in work
+      }
+      var publicAPI = {
+          login: doLogin
+      };
+      return publicAPI;
+}
+
+//create a "user" module instance
+var fred = User();
+fred.login( "fred" , "12Battery34!");
+
+/* this indentifiers - this usually points to an object, but the object depends how it was called */
+function foo(){
+        console.log( this.bar );
+}
+var bar = "global";
+var obj1 = {
+        bar: "obj1"
+        foo: foo
+};
+var obj2 = {
+        bar: "obj2"
+};
+//----
+foo();      // "global"   non-strict mode. In strict-mode it would come out undefined
+obj1.foo(); // "obj1"
+foo.call( obj2 ); //"obj2"
+new foo();   // undefined new goo() sets this to a brand new empty project
+
+/* Prototypes */
+//when you reference a property on ana object, if that object doesn't exist, JS will use that object's internal PROTOTYPE to find another object to fill in
+var foo = {
+  a: 42
+};
+//create 'bar' and link it to 'foo'
+var bar = Object.create( foo );
+bar.b = "hello world";
+
+bar.b;  //<--- "hello world"
+bar.a;  //<-- 42 assigned to foo
+
+/* New features in old browsers
+      two techniques to bring in the newer JS called
+          polyfilling and transpiling  */
+//polyfilling - taking the definition of a newer feature and providing a code that's equivalent
+//for example Number.isNaN(..) checks for NaN values in newer Es6 browsers
+if (!Number.isNaN){
+        Number.isNaN = function isNaN( x ) {
+          return x! == x;                             //NaN value is the only one that would make x !== x be true
+    };
+}
+//you can search up polyfills that are provided by other programmers
+
+//transpiling - a tool that converts newer code (syntax) into older code equivalents (older syntax)
+//new syntax is added to make your code more readable and maintainable. You SHOULD prefer to write newer and cleaner code
+//new syntax takes advantage of broswer performance optimizations
+//Allows for easier testing
+
+//Es6 adds a "default parameter value" which auto assigns values
+function foo( a = 2 ){
+      console.log( a );
+}
+foo();    // 2
+foo( 42 );  //42
+foo( 23 );  //23
+
+//older syntax to make the new feature work in older browsers
+function foo () {
+      var a = arguments[0] !== ( void 0 ) ? arguments[0] : 2;       //checks to see if the 'arguments[0]' value is 'void 0' (aka undefined), and if so provides default value 2
+      console.log( a );
+}
+
+/* non-JS code */
+var el = document.getElementById( "foo" );
+//the 'document' variable exists as a global variable when your code is running in a broswer
+// a special 'object' called a "host object"
+// 'getElementById' is not JS, but an interface to a built-in method provided by the DOM from your browsers
+//traditonally the DOM and its behavior is impplemented in something more like C/C++
+
+alert(..)
+//provided to your JS program by the broswer.
+
+console.log()
+//broswer
